ae96e1d08e3d45164b1427709424abda92fa70e2
DiasDia11 <106533792+DiasDia11@users.noreply.github.com>
2023-03-21 15:33:48 +0000

    O 'L' do SOLID

---
+++ app_poligonos_LSP/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codespace/app_poligonos',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '300a9d7c73785901deda0aa44a26b65b2ed2a426',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'codespace/app_poligonos' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '300a9d7c73785901deda0aa44a26b65b2ed2a426',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
